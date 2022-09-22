9e23315c3783cf682284cce2736e08a92d315b5e
noxter <mirko.dalfarra@gmail.com>
2022-09-22 23:14:55 +0200

    Initial and maybe final commit.
    Until next time Dockerfile!

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import TextBox from './components/TextBox.vue'
+</script>
+
+<template>
+  <div id="container">
+    <TextBox
+      title="Pineapple pizza hater"
+      description="Hating pineapple over pizza since 2002"
+    />
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/components/TextBox.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  defineProps<{
+    title: string
+    description: string
+  }>()
+
+  const count = ref(0)
+</script>
+
+<template>
+  <div className="text-wrap">
+    <h1 className="title">{{ title }}</h1>
+    <p className="description"> {{ description }} </p>
+  </div>
+</template>
+
+<style>
+</style>
